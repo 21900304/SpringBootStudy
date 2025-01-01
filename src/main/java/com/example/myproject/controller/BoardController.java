bfd8b06ad6f56d0e678ff0ddf8fa23730b422421
21900304 <dkgn000@naver.com>
2025-01-01 23:59:28 +0900

    validator

--- src/main/java/com/example/myproject/controller/BoardController.java
+++ src/main/java/com/example/myproject/controller/BoardController.java
@@ -2,11 +2,15 @@ package com.example.myproject.controller;
 
 import com.example.myproject.model.Board;
 import com.example.myproject.repository.BoardRepository;
+import com.example.myproject.validator.BoardValidator;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.constraints.Size;
+
 
 import java.util.List;
 
@@ -18,11 +22,36 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model) {
 
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+
+        if(id == null) {
+            model.addAttribute("board", new Board());
+
+        }else {
+            Board board = boardRepository.findById(id).orElse(null);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/example/myproject/model/Board.java
+++ src/main/java/com/example/myproject/model/Board.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
+//import org.antlr.v4.runtime.misc.Size;
 
 @Entity
 @Data
@@ -12,6 +15,8 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min = 2, max = 50)
     private String title;
     private String content;
 }
